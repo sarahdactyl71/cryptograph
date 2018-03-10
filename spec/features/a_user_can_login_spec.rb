84b545ebd8f4e18fb48637c2014fbd5c8771161c
Sarah Kirk <kirkse710@gmail.com>
2018-03-10 12:50:38 -0800

    adds user creation test

--- spec/features/a_user_can_login_spec.rb
+++ spec/features/a_user_can_login_spec.rb
@@ -1,18 +1,30 @@
 require 'rails_helper'
 
-RSpec.feature "a user can log in" do
+RSpec.feature "" do
+
+  it "A user can be created" do
+    visit new_user_path
+
+    fill_in "Username", with: 'Finn the Human'
+    fill_in "Password", with: 'password'
+    click_button "Create Account"
+
+    assert page.has_content?("Welcome Finn the Human")
+  end
+
   it "user is routed to homepage after login" do
-    user = User.create(username: "Sarah", password: "password")
+    user = User.create(username: "Jake the Dog", password: "password")
 
     visit login_path
     fill_in "Username", with: 'user.username'
     fill_in "Password", with: 'password'
     click_button "Login"
 
-    assert page.has_content?("Welcome Sarah")
+    assert page.has_content?("Welcome Jake the Dog")
 
     click_link "Logout"
 
-    refute page.has_content?("Welcome Sarah")
+    refute page.has_content?("Welcome Jake the Dog")
   end
+
 end
