e9d9e1b43a39f2c35423756bc35d5a94f1cc9d4a
Sarah Kirk <kirkse710@gmail.com>
2018-03-11 16:40:25 -0700

    adds more session data, makes a user can logout test

--- spec/features/a_user_can_login_spec.rb
+++ spec/features/a_user_can_login_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.feature "User information" do
+  before(:each) do
+    reset_session!
+  end
 
   it "A user can be created" do
     visit new_user_path
@@ -21,10 +24,21 @@ RSpec.feature "User information" do
     click_button "Login"
 
     assert page.has_content?("Welcome Jake the Dog")
+  end
+
+  it "a registered user can logout" do
+    user = User.create(username: "Princess Bubblegum", password: "password")
+
+    visit login_path
+    fill_in "Username", with: 'user.username'
+    fill_in "Password", with: 'password'
+    click_button "Login"
+
+    assert page.has_content?("Welcome Princess Bubblegum")
 
     click_link "Logout"
 
-    refute page.has_content?("Welcome Jake the Dog")
+    refute page.has_content?("Welcome Princess Bubblegum")
   end
 
 end
