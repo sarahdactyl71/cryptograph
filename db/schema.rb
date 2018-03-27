e044d0fc767e28e8ddcf471f9ce43578c0df40a7
Sarah Kirk <kirkse710@gmail.com>
2018-03-27 11:52:09 -0700

    adds variable to become method within alphanumeric

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180310211050) do
+ActiveRecord::Schema.define(version: 20180327184328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "monoalphabetics", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
