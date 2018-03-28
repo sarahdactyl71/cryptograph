25d3b5d2368ec463ec293e1ae688f259c9452318
Sarah Kirk <kirkse710@gmail.com>
2018-03-28 16:29:49 -0700

    adds all the atbash gravity falls ciphers for fun

--- spec/models/atbash_spec.rb
+++ spec/models/atbash_spec.rb
@@ -36,4 +36,55 @@ RSpec.describe Atbash, type: :model do
     expect(secret).to eq("zyx")
   end
 
+  #gravity falls ciphers
+
+  it "can decode cipher from Double Dipper" do
+    a = Atbash.create
+    message = a.encode("KZKVI QZN WRKKVI HZBH: 'ZFFTSDCJSTZWHZWFS!'")
+
+    expect(message).to eq("paper jam dipper says: 'auughwxqhgadsaduh!'")
+  end
+
+  it "can decode cipher from Irrational Treasure" do
+    a = Atbash.create
+    message = a.encode("V. KOFIRYFH GIVNYOVB.")
+
+    expect(message).to eq("e. pluribus trembley.")
+  end
+
+  it "can decode cipher from The Time Traveler's Pig" do
+    a = Atbash.create
+    message = a.encode("MLG S.T. DVOOH ZKKILEVW.")
+
+    expect(message).to eq("not h.g. wells approved.")
+  end
+
+  it "can decode cipher from Fight Fighters" do
+    a = Atbash.create
+    message = a.encode("HLIIB, WRKKVI, YFG BLFI DVMWB RH RM ZMLGSVI XZHGOV.")
+
+    expect(message).to eq("sorry, dipper, but your wendy is in another castle.")
+  end
+
+  it "can decode cipher from Little Dipper" do
+    a = Atbash.create
+    message = a.encode("GSV RMERHRYOV DRAZIW RH DZGXSRMT.")
+
+    expect(message).to eq("the invisible wizard is watching.")
+  end
+
+  it "can decode cipher from Summerween" do
+    a = Atbash.create
+    message = a.encode("YILFTSG GL BLF YB SLNVDLIP: GSV XZMWB.")
+
+    expect(message).to eq("brought to you by homework: the candy.")
+  end
+
+  it "can decode cipher from Boss Mabel" do
+    a = Atbash.create
+    message = a.encode("SVZEB RH GSV SVZW GSZG DVZIH GSV UVA.")
+
+    expect(message).to eq("heavy is the head that wears the fez.")
+  end
+
 end
