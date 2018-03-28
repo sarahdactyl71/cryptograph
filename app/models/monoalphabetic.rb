37a0cffe5fc151bfd512ac0b41a05c4c02bb44d9
Sarah Kirk <kirkse710@gmail.com>
2018-03-28 09:50:08 -0700

    extracts character map from monoalpabetic into a module

--- app/models/monoalphabetic.rb
+++ app/models/monoalphabetic.rb
@@ -1,36 +1,5 @@
 class Monoalphabetic < ApplicationRecord
 
-  def self.alphabet
-    {
-      a: 1,
-      b: 2,
-      c: 3,
-      d: 4,
-      e: 5,
-      f: 6,
-      g: 7,
-      h: 8,
-      i: 9,
-      j: 10,
-      k: 11,
-      l: 12,
-      m: 13,
-      n: 14,
-      o: 15,
-      p: 16,
-      q: 17,
-      r: 18,
-      s: 19,
-      t: 20,
-      u: 21,
-      v: 22,
-      w: 23,
-      x: 24,
-      y: 25,
-      z: 26
-            }
-  end
-
   def scramble(message, num)
     message = message.chars
     message.map! do |letter|
