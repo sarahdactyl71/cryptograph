33580cc33c110237c1065920e5527ae1be563c4b
Sarah Kirk <kirkse710@gmail.com>
2018-03-30 16:48:33 -0700

    begins writing encode method in Alphanumeric

--- app/models/alphanumeric.rb
+++ app/models/alphanumeric.rb
@@ -1,4 +1,14 @@
 class Alphanumeric < ApplicationRecord
   include CharacterMap
-  
+
+  def encode(message)
+    message = message.chars
+    secret = ""
+    message.each do |letter|
+      letter = self.character_map.index(letter).to_i + 1
+      secret << letter
+    end
+    secret
+  end
+
 end
