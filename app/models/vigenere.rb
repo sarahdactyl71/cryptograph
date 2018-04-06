b0b5c064b2f97e3bcaa0286be09ff4d78e7d0efd
Sarah Kirk <kirkse710@gmail.com>
2018-04-06 16:30:48 -0700

    begins fleshing out vigenere methods

--- app/models/vigenere.rb
+++ app/models/vigenere.rb
@@ -1,10 +1,18 @@
 class Vigenere < ApplicationRecord
 
   def encode(message, keyword)
-    message_length = 
+    binding.pry
+    message_legnth = message.length
+    keyword = keyword_length(keyword, message_legnth)
   end
 
   def decode(message, keyword)
+  end
+
+  def keyword_length(keyword. message_legnth)
+    message_legnth.times do
+      
+    end
   end
 
 end
