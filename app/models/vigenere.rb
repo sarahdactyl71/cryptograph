792eed82ddcb5be85723b7b89cccf856684803e2
Sarah Kirk <kirkse710@gmail.com>
2018-04-06 16:45:41 -0700

    reworks encode and creates an additional method

--- app/models/vigenere.rb
+++ app/models/vigenere.rb
@@ -1,18 +1,16 @@
 class Vigenere < ApplicationRecord
 
   def encode(message, keyword)
-    binding.pry
     message_length = message.length
-    keyword = keyword_length(keyword, message_length)
+    new_keyword = keyword_length(keyword, message_length)
   end
 
   def decode(message, keyword)
   end
 
-  def keyword_length(keyword. message_length)
-    until keyword.length == message_length
-      binding.pry
-    end
+  def keyword_length(keyword, message_length)
+    left_over = message_length % keyword.length
+    binding.pry
   end
 
 end
