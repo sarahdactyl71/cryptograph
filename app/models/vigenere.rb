d8c797ec70a409982526aecbb1e15596615cf40b
Sarah Kirk <kirkse710@gmail.com>
2018-04-06 16:35:03 -0700

    fixes spelling error for message_length"

--- app/models/vigenere.rb
+++ app/models/vigenere.rb
@@ -2,16 +2,16 @@ class Vigenere < ApplicationRecord
 
   def encode(message, keyword)
     binding.pry
-    message_legnth = message.length
-    keyword = keyword_length(keyword, message_legnth)
+    message_length = message.length
+    keyword = keyword_length(keyword, message_length)
   end
 
   def decode(message, keyword)
   end
 
-  def keyword_length(keyword. message_legnth)
-    message_legnth.times do
-      
+  def keyword_length(keyword. message_length)
+    until keyword.length == message_length
+      binding.pry
     end
   end
 
