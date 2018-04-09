63a46087f3c040538f44518190425453ba9e3a68
Sarah Kirk <kirkse710@gmail.com>
2018-04-09 16:42:58 -0700

    updates keyword_length method to get the proper number of characters in new keyword

--- app/models/vigenere.rb
+++ app/models/vigenere.rb
@@ -3,6 +3,7 @@ class Vigenere < ApplicationRecord
   def encode(message, keyword)
     message_length = message.length
     new_keyword = keyword_length(keyword, message_length)
+    binding.pry
   end
 
   def decode(message, keyword)
@@ -10,7 +11,10 @@ class Vigenere < ApplicationRecord
 
   def keyword_length(keyword, message_length)
     left_over = message_length % keyword.length
-    binding.pry
+    until keyword.length > message_length do
+      keyword += keyword
+    end
+    keyword[0..-left_over]
   end
 
 end
